c0d0059b2969027a7ac9a938b6ae4e40d612dba5
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-02-19 20:36:22 -0300

    first commit

---
+++ src/main/java/com/giacomini/estimateshub/EstimatesHubApplication.java
@@ -0,0 +1,13 @@
+package com.giacomini.estimateshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimatesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimatesHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giacomini/estimateshub/EstimatesHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giacomini.estimateshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimatesHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
